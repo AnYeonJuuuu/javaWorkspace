ac737b633f3cfd3818c23b70db4089211248669a
ayj71 <ayj7137@naver.com>
2022-06-01 22:42:05 +0900

    01print

---
+++ javaPrj01print/src/ppp/Main.java
@@ -0,0 +1,17 @@
+package ppp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 한 줄 주석
+		//여러줄 주석 작성법 /* */ 
+		
+		System.out.println("hello world ~ ! ^~^");
+		System.out.println("안녕하세요 안연주 입니다.");
+		System.out.println("자바 화이팅 ~~~ !");
+		//주석 처리가 되어서 메모 가능!
+	}
+
+}
---
+++ javaPrj01print/src/print/PrintTest01.java
@@ -0,0 +1,20 @@
+package print;
+
+public class PrintTest01 {
+
+	public static void main(String[] args) {
+	
+		System.out.println("\"안녕하세요, 제 아이디는 ayj7137! 입니다.\"");
+		
+		System.out.println("제 이름은 안연주 입니다. \n잘 부탁드립니다.");
+		
+		System.out.print("그냥 프린트");
+		System.out.print("그냥 프린트");
+		System.out.print("그냥 프린트");
+		//줄 바꿈 여부에 대한 차이
+		
+		
+
+	}
+
+}
