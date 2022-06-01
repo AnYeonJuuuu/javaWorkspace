7898f6d2a2529006a12603d6053cfcf57080d9d6
ayj71 <ayj7137@naver.com>
2022-06-01 22:40:47 +0900

    03operator

---
+++ javaPrj03operator/src/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 연산자
+			- 산술연산자 = 숫자와 숫자 연산 결과 = 숫자
+			
+			 : +, -, *, /(=몫을 구하는 연산자), %(=나머지 구하는 연산자), =(대입연산자!!)
+			
+			- 비교연산자 = 숫자와 숫자 연산 결과  = 논리(참, 거짓)
+			
+			: <, >, < = , > = , == , ! =(같지 않다)
+			
+			- 논리연산자 = 논리 와 논리 연산 결과 = 논리
+			
+			:  &&(and),  ||(or), !(not)
+			
+			*/
+		
+		System.out.println(10+20);
+		
+		System.out.println(10<20);
+		
+		System.out.println(true && false);
+		
+	}
+
+}
